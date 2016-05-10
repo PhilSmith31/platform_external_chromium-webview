86c9c5413d20102a7ef09d60159b98b04a364139
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-05-10 12:09:43 -0600

    webview: stable: add 50.0.2661.86 for "arm" "arm64" and "x86"
    * by default for all versions should be used stable
      and if you want to use the beta or dev (possibly in the future see the light)
      you can use the appropriate flag for them
    
    * for use beta add into your boardconfig: PREBUILT_WEBVIEW_VERSION := beta
    
    * also change the local certificate to PRESIGNED
      to avoid the system to change the file signature in building
    
    Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>

--- Android.mk
+++ Android.mk
@@ -18,19 +18,23 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifndef PREBUILT_WEBVIEW_VERSION
+    PREBUILT_WEBVIEW_VERSION := stable
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := webview
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch).apk
+LOCAL_SRC_FILES := prebuilt/$(PREBUILT_WEBVIEW_VERSION)/$(my_src_arch).apk
 
 LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
